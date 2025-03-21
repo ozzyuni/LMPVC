240c6ef86752d2c068502226e637945c6a4baa96
ozzyuni <ossi.parikka@tuni.fi>
2025-03-21 12:58:55 +0200

    temporarily adding some experiment files

---
+++ experiments/codegen_test_cli.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+import csv
+import argparse
+import threading
+import time
+import rclpy
+
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.node import Node
+from pathlib import Path
+from lmpvc_interfaces.srv import CodeGen
+
+from lmpvc_core.policy_bank import PolicyBank
+
+class CodeGenClient(Node):
+
+    def __init__(self):
+        super().__init__('code_gen_client')
+        self.group = MutuallyExclusiveCallbackGroup()
+        self.cli = self.create_client(CodeGen, 'code_gen', callback_group=self.group)
+        self.req = CodeGen.Request()
+        self.get_logger().info("Client ready!")
+    
+    def generate_inference(self, prompt: str, preamble = "", policies=""):
+        self.req.prompt = prompt
+        self.req.preamble = preamble
+        self.req.policies = policies
+
+        while not self.cli.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("Service not available, trying again...")
+
+        self.get_logger().info("Sending request...")
+        start = time.time()
+        result = self.cli.call(self.req)
+        end = time.time()
+
+        self.get_logger().info("Response received in " + str(end - start) + " seconds!")
+        return result.result
+
+def read_inputs(input_path):
+    inputs = []
+
+    with open(input_path, 'r') as input_file:
+        inputs = input_file.read().split('\n')
+
+    return inputs
+
+def write_outputs(output_path, outputs):
+
+        with open(output_path, 'w') as output_file:
+            writer = csv.writer(output_file)
+            for row in outputs:
+                writer.writerow(row)
+
+def write_to_log(log_path, entry, erase=False):
+
+    with open(log_path, 'a') as log_file:
+        if erase:
+            log_file.truncate(0)
+        log_file.write('[' + str(time.time()) + ']: ')
+        log_file.write(entry)
+
+def test(codegen, inputs, log_path):
+
+    # Prepare code generation
+    preamble = ""
+    preamble_path = Path(__file__).with_name("preamble.txt")
+    with open(preamble_path, 'r') as preamble_file:
+        preamble = preamble_file.read()
+
+    policy_bank = PolicyBank()
+
+    preamble = preamble + policy_bank.imports + policy_bank.hints
+
+    data = []
+    # Take measurements
+    for i in range(len(inputs)):
+        start = time.time()
+        result = codegen.generate_inference(inputs[i], preamble, policy_bank.definitions)
+        end = time.time()
+
+        data.append(end-start)
+        entry = str(i) + ' --- ' + inputs[i] + result + '\n'
+        write_to_log(log_path, entry)
+        time.sleep(0.5)
+    
+    return data
+
+
+def main():
+
+    # Get necessary files as arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_path')
+    parser.add_argument('output_path')
+    parser.add_argument('-i', '--iterations', help="number of tests to run (optional)")
+    parser.add_argument('-l', '--log', help="log file (optional)")
+    args = parser.parse_args()
+
+    inputs = read_inputs(args.input_path)
+    outputs = [[] for i in range(len(inputs))]
+    n = int(args.iterations) if args.iterations is not None else 1
+    log_path = args.log if args.log is not None else 'log.txt'
+
+    # ROS2 setup
+    rclpy.init()
+    executor = MultiThreadedExecutor()
+    codegen = CodeGenClient()
+    executor.add_node(codegen)
+    et = threading.Thread(target=executor.spin)
+    et.start()
+
+    write_to_log(log_path, "Starting tests!\n", erase=True)
+
+    # Run tests
+    for i in range(n):
+        write_to_log(log_path, "Test #" +  str(i) + "\n")
+        data = test(codegen, inputs, log_path)
+        # Concatenate data to outputs
+        outputs = [outputs[i] + [data[i]] for i in range(len(inputs))]
+
+    write_to_log(log_path, "Writing outputs!\n")
+    write_outputs(args.output_path, outputs)
+
+    executor.shutdown()
+    codegen.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ experiments/visualize.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+import argparse
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+def process_data(data_path):
+    
+    data = None
+    mean = None
+    std = None
+
+    with open(data_path, 'r') as datafile:
+        data = np.asarray(list(csv.reader(datafile)), dtype=float)
+
+    if data is not None:
+        mean = np.zeros(data.shape[0], dtype=float)
+        std = np.zeros(data.shape[0], dtype=float)
+
+        for i in range(data.shape[0]):
+            mean[i] = np.mean(data[i])
+            std[i] = np.std(data[i])
+    
+    return mean, std
+
+def plot_data(mean, std):
+    
+    fig, axs = plt.subplots(2)
+
+    mean_index = np.linspace(1, mean.shape[0], num=mean.shape[0], dtype=int)
+    std_index = np.linspace(1, std.shape[0], num=std.shape[0], dtype=int)
+
+    axs[0].set_xticks(range(1, mean.shape[0]+1))
+    axs[0].grid(axis='y')
+    axs[0].bar(mean_index, mean, width=0.5, label="seconds")
+    axs[0].legend(["Mean (seconds)"])
+
+    axs[1].set_xticks(range(1, std.shape[0]+1))
+    axs[1].grid(axis='y')
+    axs[1].bar(std_index, std, width=0.5, color='red')
+    axs[1].legend(["Standard deviation (seconds)"])
+
+    fig.suptitle("Latency")
+    fig.savefig('res.svg')
+
+def plot_data_compact(mean, std):
+    std_max = np.round(np.max(std), decimals=4)
+    std_mean = np.round(np.mean(std), decimals=4)
+    mean_index = np.linspace(1, mean.shape[0], num=mean.shape[0], dtype=int)
+    plt.bar(mean_index, mean, color = "#4CAF50", width=0.8)
+    plt.ylabel("latency (seconds)", fontsize = 13)
+    plt.text(29, 3.8, "std: mean = " + str(std_mean) + ", max = " + str(std_max), fontsize=13)
+    plt.gcf().set_size_inches(10, 2)
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    ax.set_xlim([0,51])
+    plt.savefig('res.svg', bbox_inches='tight')
+
+def plot_data_compact_overlay(mean, std):
+    std_max = np.round(np.max(std), decimals=4)
+    std_mean = np.round(np.mean(std), decimals=4)
+    mean_index = np.linspace(1, mean.shape[0], num=mean.shape[0], dtype=int)
+    plt.bar(mean_index, mean, color = "#4CAF50", width=0.8)
+    plt.bar(mean_index, std, width=0.8)
+    plt.ylabel("latency (seconds)", fontsize = 13)
+    plt.text(29, 3.8, "std: mean = " + str(std_mean) + ", max = " + str(std_max), fontsize=13)
+    plt.gcf().set_size_inches(10, 2)
+    ax = plt.gca()
+    ax.get_xaxis().set_visible(False)
+    ax.set_xlim([0,51])
+    plt.savefig('res.svg', bbox_inches='tight')
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_path')
+    args = parser.parse_args()
+
+    data_path = args.data_path
+
+    (mean, std) = process_data(data_path)
+
+    if mean is not None and std is not None:
+        plot_data_compact_overlay(mean, std)
+    else:
+        print("Failed to process data!")
+
+if __name__ == '__main__':
+    main()
