f7880d002493d70226d4648e13fb36b70b643655
parikkao <ossi.parikka@tuni.fi>
2025-08-13 10:38:09 +0300

    docker fixes and complete compose setup

--- lmpvc_ros2/src/setup_scripts/lmpvc_init.bash
+++ lmpvc_ros2/src/setup_scripts/lmpvc_init.bash
@@ -5,3 +5,7 @@ wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_GB/norther
 
 wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_GB/jenny_dioco/medium/en_GB-jenny_dioco-medium.onnx -P ${PWD}/models/voices/
 wget https://huggingface.co/rhasspy/piper-voices/resolve/v1.0.0/en/en_GB/jenny_dioco/medium/en_GB-jenny_dioco-medium.onnx.json -P ${PWD}/models/voices/
+
+. /opt/ros/humble/setup.bash && colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release --symlink-install
+
+cp ${PWD}/src/setup_scripts/lmpvc_ws.bash ${PWD}/lmpvc_ws.bash
